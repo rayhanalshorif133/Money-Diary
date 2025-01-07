191ef38a32b1da56ec8899976fc9ef24309e229d
rayhanalshorif133 <rayhanalshorif@gmail.com>
2025-01-07 19:13:39 +0600

    updated

--- script.js
+++ script.js
@@ -12,13 +12,8 @@ $(document).ready(function () {
       return;
     }
 
-    // Step 1: Calculate the total cost for buying the shares
     const totalBuyCost = buyPrice * quantity;
-
-    // Step 2: Calculate the total revenue from selling the shares
     const totalSellRevenue = sell_price * quantity;
-
-    // Step 3: Calculate the commission on buying and selling transactions
     const commissionOnBuy = totalBuyCost * (commissionRate / 100);
     const commissionOnSell = totalSellRevenue * (commissionRate / 100);
 
@@ -30,6 +25,20 @@ $(document).ready(function () {
     $("#sell_price").text("Total Sell Price: " + totalSellRevenue.toFixed(2) + " Taka");
     $("#commissionBuy").text("Commission on Buy: " + commissionOnBuy.toFixed(2) + " Taka");
     $("#commissionSell").text("Commission on Sell: " + commissionOnSell.toFixed(2) + " Taka");
+    $("#profitLoss").text("Net Profit/Loss: " + netProfitLoss.toFixed(2) + " Taka");
+  });
+
+
+  $("#profitLoss").click(function(){
+    const buyPrice = parseFloat($("#buyPrice").val());
+    const sell_price = parseFloat($("#sellPrice").val());
+    const quantity = parseInt($("#quantity").val());
+    const commissionRate = parseFloat($("#commission").val());
+    const totalBuyCost = buyPrice * quantity;
+    const totalSellRevenue = sell_price * quantity;
+    const commissionOnBuy = totalBuyCost * (commissionRate / 100);
+    const commissionOnSell = totalSellRevenue * (commissionRate / 100);
+    const netProfitLoss = totalSellRevenue - totalBuyCost - commissionOnSell;
     $("#profitLoss").text("Net Profit/Loss: " + netProfitLoss.toFixed(2) + " Taka");
   });
 
@@ -69,17 +78,19 @@ $(document).ready(function () {
 
 
   $(".sellPriceAutoBtn").click(function () {
-    $(this).text('Processing...');
+    $(this).text('Stop');
 
     if($(this).hasClass('process')){
       clearInterval(handleAutoID);
+      $(this).text('Auto');
+      $(this).removeClass('process bg-danger').addClass('bg-success');
       return false;
     }
 
     handleAutoID = setInterval(() => {
       handleAuto();
     }, 3000);
-    $(this).addClass('process');
+    $(this).addClass('process bg-danger').removeClass('bg-success');
   });
 
 
