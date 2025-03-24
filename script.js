bc308d912faf7094206ace5efbdaad79c314a28d
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2025-03-24 16:36:07 +0600

    insert dynamic

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 $(() => {
 
+  var index = 0;
+
   $("#btn-submit").click(() => {
 
-    const company_name = $('#company_name').val();
+    index++;
 
-    console.log(company_name);
-
-    const HTML = $(".demo-table").html();
-    $(".insert-table").append(HTML);
+    const company_name = $('#company_name').val() ? $('#company_name').val() : 'Profit Calculation';
+    var HTML = `
+        <table class="table mt-3 table-bordered table-hover">
+          <thead>
+            <tr class="bg-success text-white">
+              <th colspan="6" class="company_name">${company_name}</th>
+            </tr>
+            <tr class="bg-success text-white">
+              <th>Quantity</th>
+              <th>Cost Price</th>
+              <th>Total Cost</th>
+              <th>Selling Price</th>
+              <th>Total Revenue</th>
+              <th>Profit</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+    `;
+    $('.insert-table').prepend(HTML);
   });
 
 });
