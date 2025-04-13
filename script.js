20f7267c79ce3beb060d6eb038936c8b19dc84fb
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2025-04-13 17:07:20 +0600

    re fixed the price of cost price

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ $(() => {
 
   $('#cost_price').on('input', function () {
     var cost_price = $('#cost_price').val() ? $('#cost_price').val() : 0;
-    cost_price = roundToNearest(cost_price, 0.1);
-    cost_price = parseFloat(cost_price.toFixed(2));
-    $('#selling_price').val(cost_price);
+    sale_price = roundToNearest(cost_price, 0.1);
+    sale_price = parseFloat(sale_price.toFixed(2));
+    $('#selling_price').val(sale_price);
   });
 
   $("#btn-submit").click(() => {
@@ -19,9 +19,9 @@ $(() => {
     const company_name = $('#company_name').val() ? $('#company_name').val() : 'Profit Calculation';
     var quantity = $('#quantity').val() ? $('#quantity').val() : 0;
     var cost_price = $('#cost_price').val() ? $('#cost_price').val() : 0;
-    cost_price = roundToNearest(cost_price, 0.1);
-    cost_price = parseFloat(cost_price.toFixed(2));
-    $('#selling_price').val(cost_price);
+    sale_price = roundToNearest(cost_price, 0.1);
+    sale_price = parseFloat(sale_price.toFixed(2));
+    $('#selling_price').val(sale_price);
     var selling_price = $('#selling_price').val() ? $('#selling_price').val() : 0;
     var counting = $('#counting').val() ? $('#counting').val() : 10;
 
