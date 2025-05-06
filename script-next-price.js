0775123d408182ede78b6ca55d31f47d0ae91e1c
rayhanalshorif133 <rayhanalshorif@gmail.com>
2025-05-06 22:41:06 +0600

    updated next calculate

--- script-next-price.js
+++ script-next-price.js
@@ -2,15 +2,49 @@ $(() => {
 
 
     $("#btn-submit").click(() => {
-        index++;
-        const company_name = $('#company_name').val() ? $('#company_name').val() : 'Profit Calculation';
-        var quantity = $('#quantity').val() ? $('#quantity').val() : 0;
-        var cost_price = $('#cost_price').val() ? $('#cost_price').val() : 0;
-        sale_price = roundToNearest(cost_price, 0.1);
-        sale_price = parseFloat(sale_price.toFixed(2));
-        $('#selling_price').val(sale_price);
-        var selling_price = $('#selling_price').val() ? $('#selling_price').val() : 0;
-        var counting = $('#counting').val() ? $('#counting').val() : 10;
+        console.clear();
+        var index = 19;
+        console.log(index);
+        const company_name = $('#company_name').val() ? $('#company_name').val() : 'Next Price Calculation';
+        const oldUnits = parseFloat($('#already_quantity').val()) || 0;
+        const oldCostPerUnit = parseFloat($('#cost_price').val()) || 0;
+        const newInvestment = parseFloat($('#new_investment').val()) || 0;
+        const currentPrice = parseFloat($('#current_price').val()) || 0;
+        const newCostPerUnit = calculateNewCostPerUnit(oldUnits, oldCostPerUnit, newInvestment, currentPrice);
+        console.log(`New Cost Per Unit: ${newCostPerUnit}`);
+
+        var HTML = `<table class="table mt-3 table-bordered table-hover">
+  <thead>
+    <tr class="bg-navy text-white">
+      <th colspan="5" class="company_name">
+        ${company_name}
+        <div class="close"><i class="fa-solid fa-xmark"></i></div>
+      </th>
+    </tr>
+  </thead>
+  <tbody class="bg-success text-white">
+    <tr><td colspan="3">Owned Units</td><td colspan="2">${oldUnits}</td></tr>
+    <tr><td colspan="3">Cost/Unit</td><td colspan="2">${oldCostPerUnit}</td></tr>
+    <tr><td colspan="3">Market Price</td><td colspan="2">${currentPrice}</td></tr>
+    <tr><td colspan="3">New Investment</td><td colspan="2">${newInvestment}</td></tr>
+    <tr><td colspan="3">Total Cost</td><td colspan="2">${(oldUnits * oldCostPerUnit) + newInvestment}</td></tr>
+    <tr><td colspan="3">Total Units</td><td colspan="2">${oldUnits + parseInt(newInvestment / currentPrice)}</td></tr>
+    <tr><td colspan="3">New Average Cost</td><td colspan="2">${newCostPerUnit}</td></tr>
+  </tbody>
+</table>
+`;
+
+        $(".demo-table").html(HTML);
     });
 
 });
+
+
+const calculateNewCostPerUnit = (oldUnits, oldCostPerUnit, newInvestment, currentPrice) => {
+    const oldTotalCost = oldUnits * oldCostPerUnit;
+    const newUnits = newInvestment / currentPrice;
+    const totalCost = oldTotalCost + newInvestment;
+    const totalUnits = oldUnits + newUnits;
+    const newAverageCost = totalCost / totalUnits;
+    return parseFloat(newAverageCost.toFixed(2));
+}
