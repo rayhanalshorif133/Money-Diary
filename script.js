f40dd4b9b165cdf3502679871c731f8b0f5de2c9
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2025-07-28 14:56:48 +0600

    reverse

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
 $(() => {
 
-  // // http://127.0.0.1:5650/index.html?from=next_page&buy_unit_qty=2281&per_unit_price=3.11
 
-  var index = 0;
+
+
+  $("#btn-submit").click(() => {
+    console.clear();
+    const company_name = $('#company_name').val() ? $('#company_name').val() : 'Next Price Calculation';
+    const oldUnits = parseFloat($('#already_quantity').val()) || 0;
+    const oldCostPerUnit = parseFloat($('#cost_price').val()) || 0;
+    const newInvestment = parseFloat($('#new_investment').val()) || 0;
+    const currentPrice = parseFloat($('#current_price').val()) || 0;
+    const newCostPerUnit = calculateNewCostPerUnit(oldUnits, oldCostPerUnit, newInvestment, currentPrice);
+    const totalUnit = oldUnits + parseInt(newInvestment / currentPrice);
+
+    var HTML = `<table class="table mt-3 table-bordered table-hover">
+          <thead>
+              <tr class="bg-navy text-white">
+                <th colspan="5" class="company_name">
+                  ${company_name}
+                  <div class="close"><i class="fa-solid fa-xmark"></i></div>
+                </th>
+              </tr>
+          </thead>
+          <tbody class="bg-success text-white">
+            <tr><td colspan="3">Owned Units</td><td colspan="2">${oldUnits}</td></tr>
+            <tr><td colspan="3">Cost/Unit</td><td colspan="2">${oldCostPerUnit}</td></tr>
+            <tr><td colspan="3">Market Price</td><td colspan="2">${currentPrice}</td></tr>
+            <tr><td colspan="3">New Investment</td><td colspan="2">${newInvestment}</td></tr>
+            <tr><td colspan="3">New Unit</td><td colspan="2">${parseInt(newInvestment / currentPrice)}</td></tr>
+            <tr><td colspan="3">Total Cost</td><td colspan="2">${(oldUnits * oldCostPerUnit) + newInvestment}</td></tr>
+            <tr><td colspan="3">Total Units</td><td colspan="2">${totalUnit}</td></tr>
+            <tr><td colspan="3">New Average Cost</td><td colspan="2">${newCostPerUnit}</td></tr>
+          </tbody>
+        </table>
+        `;
+
+    $(".demo-table").html(HTML);
+    $("#priceBtn").attr("href", `./price.html?from=next_page&company_name=${company_name}&buy_unit_qty=${totalUnit}&per_unit_price=${newCostPerUnit}`);
+
+    drawTable(company_name, totalUnit, newCostPerUnit);
+  });
+
+});
+
+
+const drawTable = (company_name = 'New Company', quantity = 0, cost_price = 0) => {
 
   const commision = 0.0045;
 
+  sale_price = roundToNearest(cost_price, 0.1);
+  sale_price = parseFloat(sale_price.toFixed(2));
+  var selling_price = sale_price ? sale_price : 0;
+  var counting = $('#counting').val() ? $('#counting').val() : 50;
 
-  $('#cost_price').on('input', function () {
-    var cost_price = $('#cost_price').val() ? $('#cost_price').val() : 0;
-    sale_price = roundToNearest(cost_price, 0.1);
-    sale_price = parseFloat(sale_price.toFixed(2));
-    $('#selling_price').val(sale_price);
-  });
+  quantity = parseFloat(quantity);
+  cost_price = parseFloat(cost_price);
+  selling_price = parseFloat(selling_price);
+  counting = parseInt(counting);
 
-  $("#btn-submit").click(() => {
+  // ey porjnto OK 
 
-    index++;
-
-    const company_name = $('#company_name').val() ? $('#company_name').val() : 'Profit Calculation';
-    var quantity = $('#quantity').val() ? $('#quantity').val() : 0;
-    var cost_price = $('#cost_price').val() ? $('#cost_price').val() : 0;
-    sale_price = roundToNearest(cost_price, 0.1);
-    sale_price = parseFloat(sale_price.toFixed(2));
-    $('#selling_price').val(sale_price);
-    var selling_price = $('#selling_price').val() ? $('#selling_price').val() : 0;
-    var counting = $('#counting').val() ? $('#counting').val() : 10;
-
-    quantity = parseFloat(quantity);
-    cost_price = parseFloat(cost_price);
-    selling_price = parseFloat(selling_price);
-    counting = parseInt(counting);
-
-
-    var HTML = `
+  var HTML = `
         <table class="table mt-3 table-bordered table-hover">
           <thead>
             <tr class="bg-success text-white">
@@ -51,10 +78,10 @@ $(() => {
           </thead>
           <tbody>`;
 
-    var profit_gain = false;
-    var perCommission = selling_price * commision;
+  var profit_gain = false;
+  var perCommission = selling_price * commision;
 
-    HTML += `
+  HTML += `
         <tr class="bg-navy text-white">
               <td colspan="6">
               <span class="text-white">Quantity: </span>${quantity} (${parseFloat(perCommission.toFixed(2))})
@@ -62,19 +89,19 @@ $(() => {
             </tr>
         `;
 
-    for (let index = 0; index < counting; index++) {
-      var comm = quantity * selling_price * commision;
-      var index_num = index + 1;
-      var profit = ((quantity * selling_price) - (quantity * cost_price)) - comm;
+  for (let index = 0; index < counting; index++) {
+    var comm = quantity * selling_price * commision;
+    var index_num = index + 1;
+    var profit = ((quantity * selling_price) - (quantity * cost_price)) - comm;
 
-      var sale_per_unit = (quantity * selling_price - comm) / quantity;
-
-
-      var total_cost = (quantity * cost_price).toFixed(2);
-      var total_revenue = (quantity * selling_price).toFixed(2);
+    var sale_per_unit = (quantity * selling_price - comm) / quantity;
 
 
-      HTML += `
+    var total_cost = (quantity * cost_price).toFixed(2);
+    var total_revenue = (quantity * selling_price).toFixed(2);
+
+
+    HTML += `
         <tr data-info="${total_cost}-${total_revenue}" class="${!profit_gain && profit > 0 ? 'profit-gain' : ''}">
               <td>${index_num}</td>
               <td>${cost_price}</td>
@@ -83,60 +110,55 @@ $(() => {
               <td>${profit.toFixed(2)}</td>
             </tr>
         `;
-      selling_price += 0.1;
-      selling_price = parseFloat(selling_price.toFixed(2));
-      if (profit > 0) {
-        profit_gain = true;
-      }
+    selling_price += 0.1;
+    selling_price = parseFloat(selling_price.toFixed(2));
+    if (profit > 0) {
+      profit_gain = true;
     }
-
-    HTML += `</tbody></table>`;
-    $('.insert-table').prepend(HTML);
-  });
-
-  // check params and auto set
-  const url = new URL(window.location.href);
-  if (url.searchParams.get('from') === 'next_page') {
-    const company_name = url.searchParams.get('company_name');
-    const buy_unit_qty = url.searchParams.get('buy_unit_qty');
-    const per_unit_price = url.searchParams.get('per_unit_price');
-    $('#company_name').val(company_name);
-    $('#quantity').val(buy_unit_qty);
-    $('#cost_price').val(per_unit_price);
-    var sale_price = roundToNearest(per_unit_price, 0.1);
-    sale_price = parseFloat(sale_price.toFixed(2));
-    $('#selling_price').val(sale_price);
-    $("#btn-submit").click();
   }
 
+  HTML += `</tbody></table>`;
+  $('.insert-table').prepend(HTML);
+
+
+};
+
+
+const calculateNewCostPerUnit = (oldUnits, oldCostPerUnit, newInvestment, currentPrice) => {
+  // Calculate new average cost per unit
+  const oldTotalCost = oldUnits * oldCostPerUnit;
+  const newUnits = newInvestment / currentPrice;
+  const totalCost = oldTotalCost + newInvestment;
+  const totalUnits = oldUnits + newUnits;
+  const newAverageCost = totalCost / totalUnits;
+  const newCostPerUnit = parseFloat(newAverageCost.toFixed(2));
+  return newCostPerUnit;
+}
+
+
+$("#btn-reset").click(() => {
+  const url = new URL(window.location.href);
+  window.history.replaceState(null, '', url.pathname);
+  location.reload();
+});
 
 
 
+$(document).on('click', 'tr', function () {
+  $(this).addClass('selected').siblings().removeClass('selected');
 
+  if(!$(this).data('info')){
+    return false;
+  }
 
+  var dataInfo = $(this).data('info');
+  var values = dataInfo.split('-');
+  var totalCost = parseFloat(values[0]);
+  var totalRevenue = parseFloat(values[1]);
 
-  $("#btn-reset").click(() => {
-    const url = new URL(window.location.href);
-    window.history.replaceState(null, '', url.pathname);
-    location.reload();
-  });
+  $(this).siblings().find('.remove-container').remove();
 
-
-  $(document).on('click', '.close', function () {
-    $(this).closest('table').remove();
-  });
-
-  $(document).on('click', 'tr', function () {
-    $(this).addClass('selected').siblings().removeClass('selected');
-
-    var dataInfo = $(this).data('info');
-    var values = dataInfo.split('-');
-    var totalCost = parseFloat(values[0]);
-    var totalRevenue = parseFloat(values[1]);
-
-    $(this).siblings().find('.remove-container').remove();
-
-    $(this).after(`<tr><td colspan="5" class="text-center remove-container">
+  $(this).after(`<tr><td colspan="5" class="text-center remove-container">
       Total Cost: 
       <strong>
         ${totalCost.toFixed(2)}</strong> | Total Revenue: <strong>${totalRevenue.toFixed(2)} | Profit: ${(totalRevenue - totalCost).toFixed(2)}
@@ -147,13 +169,13 @@ $(() => {
       </td></tr>`);
 
 
-  });
-
-  $(document).on('click', '.remove-row', function () {
-    $(this).closest('tr').prev().remove();
-    $(this).closest('tr').remove();
-  });
 });
+
+$(document).on('click', '.remove-row', function () {
+  $(this).closest('tr').remove();
+});
+
+
 
 function roundToNearest(value, step) {
   return Math.round(value / step) * step;
