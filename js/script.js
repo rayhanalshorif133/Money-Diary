25d54b9c34e9c31abbfd81dcc51c54657160d3c5
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2025-08-19 12:33:12 +0600

    updated new design

--- js/script.js
+++ js/script.js
@@ -66,34 +66,61 @@ const submitFunction = (hasInsert = false) => {
     insertNewData(company_name,keyword, oldUnits, oldCostPerUnit, currentPrice, newInvestment, counter);
   }
 
-  return false;
 
   const totalUnit = oldUnits + parseInt(newInvestment / currentPrice);
 
-  var HTML = `<table class="table mt-3 table-bordered table-hover">
+
+  var HTML = `
+    <table>
           <thead>
-              <tr class="bg-navy text-white">
-                <th colspan="5" class="company_name">
-                  ${company_name}
-                  <div class="close"><i class="fa-solid fa-xmark"></i></div>
-                </th>
-              </tr>
+            <tr>
+              <th colspan="5" class="company_name">
+                 ${company_name}
+                <div class="close"><i class="fa-solid fa-xmark"></i></div>
+              </th>
+            </tr>
           </thead>
           <tbody class="bg-success text-white">
-            <tr><td colspan="3">Owned Units</td><td colspan="2">${oldUnits}</td></tr>
-            <tr><td colspan="3">Cost/Unit</td><td colspan="2">${oldCostPerUnit}</td></tr>
-            <tr><td colspan="3">Market Price</td><td colspan="2">${currentPrice}</td></tr>
-            <tr><td colspan="3">New Investment</td><td colspan="2">${newInvestment}</td></tr>
-            <tr><td colspan="3">New Unit</td><td colspan="2">${parseInt(newInvestment / currentPrice)}</td></tr>
-            <tr><td colspan="3">Total Cost</td><td colspan="2">${(oldUnits * oldCostPerUnit) + newInvestment}</td></tr>
-            <tr><td colspan="3">Total Units</td><td colspan="2">${totalUnit}</td></tr>
-            <tr><td colspan="3">New Average Cost</td><td colspan="2">${newCostPerUnit}</td></tr>
+            <tr>
+              <td colspan="3">Owned Units</td>
+              <td colspan="2">${oldUnits}</td>
+            </tr>
+            <tr>
+              <td colspan="3">Cost/Unit</td>
+              <td colspan="2">${oldCostPerUnit}</td>
+            </tr>
+            <tr>
+              <td colspan="3">Market Price</td>
+              <td colspan="2">${currentPrice}</td>
+            </tr>
+            <tr>
+              <td colspan="3">New Investment</td>
+              <td colspan="2">${newInvestment}</td>
+            </tr>
+            <tr>
+              <td colspan="3">New Unit</td>
+              <td colspan="2">${parseInt(newInvestment / currentPrice)}</td>
+            </tr>
+            <tr>
+              <td colspan="3">Total Cost</td>
+              <td colspan="2">${(oldUnits * oldCostPerUnit) + newInvestment}</td>
+            </tr>
+            <tr>
+              <td colspan="3">Total Units</td>
+              <td colspan="2">${totalUnit}</td>
+            </tr>
+            <tr>
+              <td colspan="3">New Average Cost</td>
+              <td colspan="2">${newCostPerUnit}</td>
+            </tr>
           </tbody>
         </table>
-        `;
+  `;
 
-  $(".demo-table").html(HTML);
+  $("#calculate-table").html(HTML);
   $("#priceBtn").attr("href", `./price.html?from=next_page&company_name=${company_name}&buy_unit_qty=${totalUnit}&per_unit_price=${newCostPerUnit}`);
+
+  return false;
 
   drawTable(company_name, totalUnit, newCostPerUnit, counter);
 }
@@ -211,6 +238,8 @@ const drawTable = (company_name = 'New Company', quantity = 0, cost_price = 0, c
 
   HTML += `</tbody></table>`;
   $('.insert-table').prepend(HTML);
+
+  // calculate-table-list
 
 
 };
